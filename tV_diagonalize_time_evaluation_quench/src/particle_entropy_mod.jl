9d2e6558fc7921a9144ddf6c45496066d5deca81
Adrian Del Maestro <adrian@delmaestro.org>
2018-06-12 10:46:11 +0200

    added computation of OBDM

--- tV_diagonalize_time_evaluation_quench/src/particle_entropy_mod.jl
+++ tV_diagonalize_time_evaluation_quench/src/particle_entropy_mod.jl
@@ -19,16 +19,16 @@ function particle_entropy_mod(basis::AbstractSzbasis, Asize::Int, d::Vector{Comp
     if Asize>Bsize
         Asize,Bsize=Bsize,Asize
     end
-    # Dimensions of partition Hilbert spaces
 
+    # Dimensions of partition Hilbert spaces
     facto=1.0
     for i=1:Asize
-	facto *=((1.0*L-i+1)/(1.0*i))
+    	facto *=((1.0*L-i+1)/(1.0*i))
     end
     DimA = Int(round(facto))
     facto=1.0
     for i=1:Bsize
-	facto *=((1.0*L-i+1)/(1.0*i))
+    	facto *=((1.0*L-i+1)/(1.0*i))
     end
     DimB = Int(round(facto))
 
@@ -47,47 +47,57 @@ function particle_entropy_mod(basis::AbstractSzbasis, Asize::Int, d::Vector{Comp
     # Normalization coefficient
     norm=sqrt(Wa*Wb/factorial(basis.N))
 
-
     for (i, braA) in enumerate(basisA)
         for (j, braB) in enumerate(basisB)
-	    bra=braA+braB
-	    if bra in basis
-		Flips=0
-		SumFlips=0
-		for k=1:L
-		    Flips = Flips +(1-2* Flips)* braA[k]
-		    SumFlips += Flips* braB[k]
-		end
-		Amatrix[i, j] = (-1)^ SumFlips*norm*d[serial_num(basis, bra)]
-	    end
-	end
+            bra=braA+braB
+	        if bra in basis
+                Flips=0
+		        SumFlips=0
+        		for k=1:L
+        		    Flips = Flips +(1-2* Flips)* braA[k]
+        		    SumFlips += Flips* braB[k]
+        		end
+        		Amatrix[i,j] = (-1)^SumFlips*norm*d[serial_num(basis, bra)]
+    	    end
+    	end
     end
 
-    AdAmatrix = zeros(Complex128, DimAdA, DimAdA)
-#print("sts\n")
-#    for i=1:DimA
-#        for j=i:DimA
-#	    AdA_elem=0.0
-#            for k=1:DimB
-#               AdA_elem += Amatrix[i,k]* conj(Amatrix[j,k])
-#	    end
-#	    AdAmatrix[i,j]= AdA_elem
-#	    AdAmatrix[j,i]= conj(AdA_elem)
-#        end
-#    end
-#print("end\n")
-#open( "Corr_pbcL22N11.dat", "w") do f
-#   write(f, "# i-j Corr_cal  Corr_th\n")
-#    for i=1:DimA
-#        Corr=0
-#        for j=1:Int((L-2)/4)
-#	    Corr+=cos(2*pi*j/L*(i-Int(L/2)))
-#        end
-#        Corr=(2*Corr+1)*2/L/L
-#        write(f, "$(i-Int(L/2)) $(AdAmatrix[i,Int(L/2)]) $(Corr)\n")
-#    end
-#flush(f)
-#end
+    # construct the spatial OBDM
+    obdm = zeros(Float64, DimA)
+
+    if Asize == 1
+        AdAmatrix = zeros(Complex128, DimAdA, DimAdA)
+
+        for i=1:DimA
+            for j=i:DimA
+        	   AdA_elem=0.0
+               for k=1:DimB
+                  AdA_elem += Amatrix[i,k]* conj(Amatrix[j,k])
+        	   end
+        	   AdAmatrix[i,j]= AdA_elem
+        	   AdAmatrix[j,i]= conj(AdA_elem)
+           end
+       end
+
+       for i=1:DimA
+           obdm[i] = real(AdAmatrix[i,Int(L/2)])
+       end
+
+   end
+
+   #print(trace(AdAmatrix))
+   #AdAmatrix = AdAmatrix/trace(AdAmatrix)
+
+   # write it to disk
+
+   # open( "Corr_pbcL14N07_int_strong.dat", "w") do f
+   #     write(f, "# i-j Corr_cal\n")
+   #     for i=1:DimA
+   #         write(f, "$(i-Int(L/2)) $(real(AdAmatrix[i,Int(L/2)])) $(Corr)\n")
+   #     end
+   #     flush(f)
+   #  end
+
     #S = svdvals!(AdAmatrix)
     S = svdvals!(Amatrix)
     S.=S.^2
@@ -107,5 +117,5 @@ function particle_entropy_mod(basis::AbstractSzbasis, Asize::Int, d::Vector{Comp
     SRen[2]=-log(sum(S.^2))-LogNn
     SRen[3]=-log(sum(S.^3))/2-LogNn
     #warn(" AdA eigenvalue are", S)
-    return SRen
+    return SRen,obdm
 end
