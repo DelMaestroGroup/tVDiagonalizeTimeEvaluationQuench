3896a94f20a7046a7b430f949f56ca71ee7edfe3
HatemBarghathi <hnb26b@mst.edu>
2018-08-20 19:18:54 -0400

    Add files via upload

---
+++ tV_diagonalize_time_evaluation_quench/src/Translational_Reflection_Symmetry_Cycles.jl
@@ -0,0 +1,42 @@
+"""
+Create a list of occupation basis for each translational and reflection symmetry cycle for fermionic 1D chains.
+"""
+function Translational_Reflection_Symmetry_Cycles(basis::AbstractSzbasis)
+
+    for x = [:NumOfCycles,:Num_cycles_max,:MemberID,:i_next]
+       @eval $x = Int64
+    end
+
+    IdStatus = Bool
+    Num_cycles_max=round(Int64,basis.D/basis.K*1.5)
+    Cycles= zeros(Int64, Num_cycles_max, basis.K)
+    CycleSize = zeros(Int64, Num_cycles_max)
+    CycleSize = zeros(Int64, Num_cycles_max)
+    CReflection= zeros(Bool, Num_cycles_max)
+    Status  = trues(basis.D)
+    NumOfCycles=0
+    for (i, bra) in enumerate(basis)
+        i_next=i
+        for j =0: 1
+            if Status[i_next]
+                NumOfCycles+=1
+                MemberID=0
+                IdStatus=true
+                while IdStatus
+                    Status[i_next]=false
+                    MemberID+=1
+                    Cycles[NumOfCycles, MemberID]= i_next
+                    i_next=serial_num(basis, circshift(basis[i_next],1))
+                    IdStatus = Status[i_next]
+                end
+                CycleSize[NumOfCycles]= MemberID
+                if j==1
+                    CReflection[NumOfCycles]=true
+                    CReflection[NumOfCycles-1]= true
+                end 
+            end
+            i_next=serial_num(basis,reverse(bra))
+        end
+    end
+    Cycles, CycleSize, NumOfCycles, CReflection
+end
