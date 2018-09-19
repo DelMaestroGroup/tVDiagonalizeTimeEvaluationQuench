476991d57796a7d55d7e5826ad016d68ba586444
HatemBarghathi <hnb26b@mst.edu>
2018-09-19 17:43:45 -0400

    Included PE_StructureMatrix function

--- tV_diagonalize_time_evaluation_quench/src/tVDiagonalize.jl
+++ tV_diagonalize_time_evaluation_quench/src/tVDiagonalize.jl
@@ -1,4 +1,4 @@
-module tVDiagonalize
+ module tVDiagonalize
 
 using JeszenszkiBasis
 
@@ -9,9 +9,12 @@ export
 
     sparse_hamiltonian,
     spatial_entropy,
-    particle_entropy_mod,
+    particle_entropy_Ts,
+    PE_StructureMatrix,
     Translational_Symmetry_Cycles,
-    Block_Diagonal_Hamiltonian,
+    Symmetry_Cycles_q0R1PH1,
+    Block_Diagonal_Hamiltonian_q0R1PH1,
+    sparse_Block_Diagonal_Hamiltonian_q0R1PH1,
     full_hamiltonian
 """
 Boundary conditions.
@@ -22,8 +25,11 @@ Boundary conditions.
 
 include("sparse_hamiltonian.jl")
 include("spatial_entropy.jl")
-include("particle_entropy_mod.jl")
+include("particle_entropy_Ts.jl")
+include("PE_StructureMatrix.jl")
 include("Translational_Symmetry_Cycles.jl")
-include("Block_Diagonal_Hamiltonian.jl")
+include("Symmetry_Cycles_q0R1PH1.jl")
+include("Block_Diagonal_Hamiltonian_q0R1PH1.jl")
+include("sparse_Block_Diagonal_Hamiltonian_q0R1PH1.jl")
 include("full_hamiltonian.jl")
 end
