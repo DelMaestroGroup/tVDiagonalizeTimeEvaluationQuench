8d32c0a5cd3ae40f9981fb482df5a69c0fbe2ae8
HatemBarghathi <hnb26b@mst.edu>
2018-09-19 18:04:30 -0400

    Including PE_StructureMatrix function

--- tV_diagonalize_time_evaluation_quench/src/tVDiagonalize.jl
+++ tV_diagonalize_time_evaluation_quench/src/tVDiagonalize.jl
@@ -10,9 +10,11 @@ export
     sparse_hamiltonian,
     spatial_entropy,
     particle_entropy_Ts,
+    PE_StructureMatrix,
     Translational_Symmetry_Cycles,
     Symmetry_Cycles_q0R1PH1,
     Block_Diagonal_Hamiltonian_q0R1PH1,
+    sparse_Block_Diagonal_Hamiltonian_q0R1PH1,
     full_hamiltonian
 """
 Boundary conditions.
@@ -24,8 +26,10 @@ Boundary conditions.
 include("sparse_hamiltonian.jl")
 include("spatial_entropy.jl")
 include("particle_entropy_Ts.jl")
+include("PE_StructureMatrix.jl")
 include("Translational_Symmetry_Cycles.jl")
 include("Symmetry_Cycles_q0R1PH1.jl")
 include("Block_Diagonal_Hamiltonian_q0R1PH1.jl")
+include("sparse_Block_Diagonal_Hamiltonian_q0R1PH1.jl")
 include("full_hamiltonian.jl")
 end
